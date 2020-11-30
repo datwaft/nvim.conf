8d24d0c96603f031d9cc7b88d1c74b7377712f56
datwaft <datwaftx@gmail.com>
2020-11-30 13:47:30 -0600

    Enhancement: ensure all plugins treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return function()
       return
    end
    configs.setup {
-      ensure_installed = "maintained",
+      ensure_installed = "all",
       highlight = {
          enable = true,
          disable = {},
