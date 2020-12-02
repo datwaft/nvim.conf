057fdb0231de52574262a6f21e27eb1873346d74
datwaft <datwaftx@gmail.com>
2020-12-02 11:21:44 -0600

    Enhancement: migration from init.vim to init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- ========
+-- INIT.LUA
+-- ========
+-- Created by: datwaft [github.com/datwaft]
+
+-- =======
+-- Plugins
+-- =======
+require('plugins')
+-- =============
+-- Configuration
+-- =============
+require('configuration')
