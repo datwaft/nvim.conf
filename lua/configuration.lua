23c59a5f9ad85011dc4ac790494fe670fef97ffe
David Guevara <datwaftx@gmail.com>
2020-12-07 11:55:35 -0600

    Enhancement: changed python path to anaconda

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -18,8 +18,8 @@ end
 -- ====================
 -- NeoVim Configuration
 -- ====================
-   vim.g.python3_host_prog = '/usr/bin/python3'
-   vim.g.python_host_prog = '/usr/bin/python'
+   vim.g.python3_host_prog = '/home/datwaft/anaconda3/bin/python3'
+   vim.g.python_host_prog = '/home/datwaft/anaconda3/bin/python'
 -- ====================
 -- Visual configuration
 -- ====================
