b16eb853aa1098ac0f513307abcfc6c92a5c8c1c
datwaft <datwaftx@gmail.com>
2021-02-22 16:16:25 -0600

    Initial version of globals

--- lua/globals.lua
+++ lua/globals.lua
@@ -5,6 +5,6 @@
 
 -- Prints parameters in a readable format
 _G.dump = function(...)
-   local objects = vim.tbl_map(vim.inspect, {...})
-   print(unpack(objects))
+  local objects = vim.tbl_map(vim.inspect, {...})
+  print(unpack(objects))
 end
