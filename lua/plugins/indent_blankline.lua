3bb36d8084bbf5ce367324e228b63bb580ebde0a
datwaft <datwaftx@gmail.com>
2021-03-25 17:29:56 -0600

    Show first level of indentation

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -6,7 +6,6 @@
 return function()
   vim.g.indent_blankline_char = '│'
   vim.g.indent_blankline_use_treesitter = true
-  vim.g.indent_blankline_show_first_indent_level = false
   vim.g.indent_blankline_filetype_exclude = {
     'help',
     'fern',
