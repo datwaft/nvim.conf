a79acd634cdd9a7e3f04abc0d2e6dfc746f6956a
datwaft <datwaftx@gmail.com>
2021-03-25 16:20:53 -0600

    Add more configuration for indent_blankline

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -5,4 +5,11 @@
 
 return function()
   vim.g.indent_blankline_char = '│'
+  vim.g.indent_blankline_use_treesitter = true
+  vim.g.indent_blankline_show_first_indent_level = false
+  vim.g.indent_blankline_filetype_exclude = {
+    'help',
+    'fern',
+    'packer',
+  }
 end
