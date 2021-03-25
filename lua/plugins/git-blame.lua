bcc6e3f7c1cb1fc38098f6fbd63e5210edef7aaf
datwaft <datwaftx@gmail.com>
2021-03-25 16:22:22 -0600

    Remove git-blame plugin configuration

--- lua/plugins/git-blame.lua
+++
@@ -1,8 +0,0 @@
--- =======================
--- GIT-BLAME CONFIGURATION
--- =======================
--- Created by datwaft <github.com/datwaft>
-
-return function()
-  vim.g.gitblame_date_format = '%Y-%m-%d'
-end
