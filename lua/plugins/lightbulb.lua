1a369d3005563512a21786e29c2d1efecdf3b7d8
datwaft <datwaftx@gmail.com>
2021-03-26 16:22:44 -0600

    Better lightbulb color

--- lua/plugins/lightbulb.lua
+++ lua/plugins/lightbulb.lua
@@ -23,5 +23,5 @@ return function()
       end
     }
   }
-  vim.api.nvim_command('highlight link LightBulbVirtualText WarningMsg')
+  vim.api.nvim_command('highlight link LightBulbVirtualText Red')
 end
