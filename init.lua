e67d0bc4ef12cc3ac230782f9363de5e7e9d2355
datwaft <datwaftx@gmail.com>
2021-10-01 14:17:30 -0600

    style: format init.lua file

--- init.lua
+++ init.lua
@@ -27,7 +27,8 @@ local packer_path = vim.fn.stdpath("data") ..
 if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
   print("Could not find packer.nvim, cloning new copy to", packer_path)
   vim.fn.system({
-    "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", packer_path
+    "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim",
+    packer_path
   })
 end
 
