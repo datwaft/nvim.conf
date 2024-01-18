3313f5548ca7adeea23ac20b94fab1f6aa4deb42
datwaft <datwaftx@gmail.com>
2024-01-17 20:41:47 -0600

    feat(formatting): add command to toggle format on save

--- lua/packages/formatting.lua
+++ lua/packages/formatting.lua
@@ -18,13 +18,41 @@ return {
     formatters = {
       latexindent = { prepend_args = { "-l", "-m" } },
     },
-    format_on_save = {
-      quiet = true,
-      lsp_fallback = true,
-      timeout = 500,
-    },
+    format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      else
+        return {
+          quiet = true,
+          lsp_fallback = true,
+          timeout = 500,
+        }
+      end
+    end,
   },
   init = function()
     vim.opt.formatexpr = [[v:lua.require("conform").formatexpr()]]
+    -- Add commands to toggle format on save
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, { desc = "Disable format on save", bang = true })
+    vim.api.nvim_create_user_command("FormatEnable", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = false
+      else
+        vim.g.disable_autoformat = false
+      end
+    end, { desc = "Enable format on save", bang = true })
+    vim.api.nvim_create_user_command("FormatToggle", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+      else
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+      end
+    end, { desc = "Toggle format on save", bang = true })
   end,
 }
