ecd82d9bb2d9359d4bc266f25efb6eea41c90263
datwaft <datwaftx@gmail.com>
2024-02-04 18:14:02 -0600

    fix(lsp/volar): have fallback for typescript server

--- lua/packages/language-servers.lua
+++ lua/packages/language-servers.lua
@@ -30,7 +30,6 @@ local function config(self, opts)
     "svelte",
     "taplo",
     "vimls",
-    "volar",
     "zls",
   }) do
     lsp[server].setup({ capabilities = capabilities })
@@ -69,6 +68,38 @@ local function config(self, opts)
       },
     },
     capabilities = capabilities,
+  })
+  -- VueJS
+  lsp.volar.setup({
+    ---@param config object
+    ---@param root_dir string
+    on_new_config = function(config, root_dir)
+      local util = require("lspconfig.util")
+      local node_global
+      do
+        local handle = io.open("npm list -g")
+        assert(handle ~= nil)
+        node_global = handle:read("*l")
+        handle:close()
+      end
+      ---@type string
+      local global_typescript = node_global .. "/node_modules/typescript/lib"
+      ---@type string
+      local local_typescript
+      ---@param path string
+      ---@return string | nil
+      local function check_dir(path)
+        local_typescript = util.path.join(path, "node_modules", "typescript", "lib")
+        if util.path.exists(local_typescript) then
+          return path
+        end
+      end
+      if util.search_ancestors(root_dir, check_dir) then
+        config.init_options.typescript.tsdk = local_typescript
+      else
+        config.init_options.typescript.tsdk = global_typescript
+      end
+    end,
   })
   -- JSON
   lsp.jsonls.setup({
