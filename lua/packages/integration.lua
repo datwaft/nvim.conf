4296292b8a5d03d0338a37167f6445b9cc953752
datwaft <datwaftx@gmail.com>
2024-02-06 19:21:58 -0600

    feat(packages): remove smart-splits.nvim
    This is because I am trying to use something other than Wezterm and TMUX and I am not ready to integrate it with Neovim yet.

--- lua/packages/integration.lua
+++ lua/packages/integration.lua
@@ -1,18 +1,4 @@
 ---@type LazySpec
 return {
-  {
-    "mrjones2014/smart-splits.nvim",
-    lazy = false,
-    ---@type fun(self: LazyPlugin, keys: string[]):string|LazyKeys[]|string[])
-    keys = function()
-      local ss = require("smart-splits")
-      return {
-        { "<C-w>h", ss.move_cursor_left },
-        { "<C-w>j", ss.move_cursor_down },
-        { "<C-w>k", ss.move_cursor_up },
-        { "<C-w>l", ss.move_cursor_right },
-      }
-    end,
-  },
   "andweeb/presence.nvim",
 }
