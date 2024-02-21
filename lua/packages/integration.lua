ea2db27328b2c7598bc7029f3f5b381df72fbcf8
datwaft <datwaftx@gmail.com>
2024-02-21 12:30:46 -0600

    feat: add integration with leetcode

--- lua/packages/integration.lua
+++ lua/packages/integration.lua
@@ -1,4 +1,15 @@
 ---@type LazySpec
 return {
+  -- Discord integration
   "andweeb/presence.nvim",
+  -- LeetCode integration
+  {
+    "kawre/leetcode.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = true,
+  },
 }
