68a18caa791dbd4f87c1855f11b0d67bf73cb3bf
datwaft <datwaftx@gmail.com>
2024-02-26 15:11:12 -0600

    feat: add vim-slime integration

--- lua/packages/integration.lua
+++ lua/packages/integration.lua
@@ -12,4 +12,16 @@ return {
     },
     config = true,
   },
+  -- REPL integration
+  {
+    "jpalardy/vim-slime",
+    init = function(self)
+      vim.g.slime_target = "tmux"
+      vim.g.slime_default_config = {
+        socket_name = vim.fn.split(vim.env.TMUX, ",")[1],
+        target_pane = "{last}",
+      }
+      vim.g.slime_dont_ask_default = 1
+    end,
+  },
 }
