b27a6606e153580ae9756f532fd6d979b72c4d41
datwaft <datwaftx@gmail.com>
2024-02-26 15:27:56 -0600

    feat: enable tmux integration (at least from neovim side)

--- lua/packages/integration.lua
+++ lua/packages/integration.lua
@@ -26,4 +26,21 @@ return {
       vim.g.slime_bracketed_paste = 1
     end,
   },
+  -- TMUX integration
+  {
+    "mrjones2014/smart-splits.nvim",
+    version = ">=1.0.0",
+    config = true,
+    lazy = false,
+    ---@type fun(self: LazyPlugin, keys: string[]):string|LazyKeys[]|string[])
+    keys = function()
+      local ss = require("smart-splits")
+      return {
+        { "<C-w>h", ss.move_cursor_left },
+        { "<C-w>j", ss.move_cursor_down },
+        { "<C-w>k", ss.move_cursor_up },
+        { "<C-w>l", ss.move_cursor_right },
+      }
+    end,
+  },
 }
