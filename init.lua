d262c820e647d975c3eaf29746478f2598bf4f17
datwaft <datwaftx@gmail.com>
2024-02-27 20:32:52 -0600

    feat(diagnostics): change signs icons
    The idea is to make all of them circles for more uniformity

--- init.lua
+++ init.lua
@@ -144,10 +144,10 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 -- Set diagnostic signs
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignHint" })
 vim.fn.sign_define("DiagnosticSignOk", { text = " ", texthl = "DiagnosticSignOk" })
 -- Keybind for showing line diagnostics
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
