159fc9e6b23ccadca5fc6d1cab2cd8ab42fb8e76
datwaft <datwaftx@gmail.com>
2024-03-13 21:11:46 -0600

    feat: autoinstall formatters using Mason but for real

--- lua/packages/formatting.lua
+++ lua/packages/formatting.lua
@@ -1,82 +1,77 @@
 ---@type LazySpec
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      css = { { "prettierd", "prettier" } },
-      html = { { "prettierd", "prettier" } },
-      javascript = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
-      json = { { "prettierd", "prettier" } },
-      jsonc = { { "prettierd", "prettier" } },
-      lua = { "stylua" },
-      tex = { "latexindent" },
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
-      vue = { { "prettierd", "prettier" } },
-    },
-    formatters = {
-      latexindent = { prepend_args = { "-l", "-m" } },
-      prettier = {
-        ---@param ctx conform.Context
-        condition = function(ctx)
-          return #vim.fs.find("biome.json", { upward = true, stop = vim.uv.os_homedir(), path = ctx.dirname }) == 0
-        end,
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        css = { { "prettierd", "prettier" } },
+        html = { { "prettierd", "prettier" } },
+        javascript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        jsonc = { { "prettierd", "prettier" } },
+        lua = { "stylua" },
+        tex = { "latexindent" },
+        typescript = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        vue = { { "prettierd", "prettier" } },
       },
-      prettierd = {
-        ---@param ctx conform.Context
-        condition = function(ctx)
-          return #vim.fs.find("biome.json", { upward = true, stop = vim.uv.os_homedir(), path = ctx.dirname }) == 0
-        end,
+      formatters = {
+        latexindent = { prepend_args = { "-l", "-m" } },
+        prettier = {
+          ---@param ctx conform.Context
+          condition = function(ctx)
+            return #vim.fs.find("biome.json", { upward = true, stop = vim.uv.os_homedir(), path = ctx.dirname }) == 0
+          end,
+        },
+        prettierd = {
+          ---@param ctx conform.Context
+          condition = function(ctx)
+            return #vim.fs.find("biome.json", { upward = true, stop = vim.uv.os_homedir(), path = ctx.dirname }) == 0
+          end,
+        },
       },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        else
+          return {
+            quiet = true,
+            lsp_fallback = true,
+            timeout = 500,
+          }
+        end
+      end,
     },
-    format_on_save = function(bufnr)
-      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-        return
-      else
-        return {
-          quiet = true,
-          lsp_fallback = true,
-          timeout = 500,
-        }
-      end
+    init = function()
+      vim.opt.formatexpr = [[v:lua.require("conform").formatexpr()]]
+      -- Add commands to toggle format on save
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, { desc = "Disable format on save", bang = true })
+      vim.api.nvim_create_user_command("FormatEnable", function(args)
+        if args.bang then
+          vim.b.disable_autoformat = false
+        else
+          vim.g.disable_autoformat = false
+        end
+      end, { desc = "Enable format on save", bang = true })
+      vim.api.nvim_create_user_command("FormatToggle", function(args)
+        if args.bang then
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
+        else
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+        end
+      end, { desc = "Toggle format on save", bang = true })
     end,
   },
-  init = function()
-    vim.opt.formatexpr = [[v:lua.require("conform").formatexpr()]]
-    -- Add commands to toggle format on save
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, { desc = "Disable format on save", bang = true })
-    vim.api.nvim_create_user_command("FormatEnable", function(args)
-      if args.bang then
-        vim.b.disable_autoformat = false
-      else
-        vim.g.disable_autoformat = false
-      end
-    end, { desc = "Enable format on save", bang = true })
-    vim.api.nvim_create_user_command("FormatToggle", function(args)
-      if args.bang then
-        vim.b.disable_autoformat = not vim.b.disable_autoformat
-      else
-        vim.g.disable_autoformat = not vim.g.disable_autoformat
-      end
-    end, { desc = "Toggle format on save", bang = true })
-  end,
-  dependencies = {
-    {
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      dependencies = { "williamboman/mason.nvim" },
-      opts = { ensure_installed = {
-        "prettierd",
-        "prettier",
-        "stylua",
-        "latexindent",
-      } },
-    },
+  {
+    "zapling/mason-conform.nvim",
+    dependencies = { "stevearc/conform.nvim", "williamboman/mason.nvim" },
+    config = true,
   },
 }
