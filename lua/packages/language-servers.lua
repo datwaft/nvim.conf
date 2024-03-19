2d4237d55ea375004e5d2bf21ec21778d6325c8d
datwaft <datwaftx@gmail.com>
2024-03-18 22:57:54 -0600

    revert: "feat(tsserver): disable suggestions"
    This reverts commit 28e0df49feb3a812e948c9007b5234c5b35d4baf.

--- lua/packages/language-servers.lua
+++ lua/packages/language-servers.lua
@@ -153,13 +153,7 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      settings = {
-        tsserver_file_preferences = {
-          disableSuggestions = true,
-        },
-      },
-    },
+    config = true,
   },
   -- Java
   {
