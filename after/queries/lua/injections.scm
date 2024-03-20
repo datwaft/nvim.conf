3eb5b169368b1743c325c01ff035af1ca9491438
datwaft <datwaftx@gmail.com>
2024-03-19 20:36:21 -0600

    feat(lua): add injection of bash for `vim.opt.grepprg`

--- after/queries/lua/injections.scm
+++ after/queries/lua/injections.scm
@@ -17,3 +17,13 @@
   (#set! injection.language "luap")
   (#eq? @_filetypeadd_identifier "vim.filetype.add")
   (#eq? @_pattern_key "pattern"))
+
+;; vim.opt.grepprg = "bash"
+((assignment_statement
+   (variable_list) @_variable_symbol
+   (expression_list
+     value:
+       (string
+         content: _ @injection.content)))
+ (#set! injection.language "bash")
+ (#eq? @_variable_symbol "vim.opt.grepprg"))
