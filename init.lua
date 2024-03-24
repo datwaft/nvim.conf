142d06764ea3267c102c4aec63bc5da585dfd20e
datwaft <datwaftx@gmail.com>
2024-03-23 18:54:37 -0600

    fix: fix pattern in query `vim.filetype.add`

--- init.lua
+++ init.lua
@@ -235,7 +235,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 -- Filetype configuration
 -------------------------
 vim.filetype.add({ filename = { ["go.mod"] = "gomod" } })
-vim.filetype.add({ pattern = { [".*/queries/.*/*.%.scm"] = "query" } })
+vim.filetype.add({ pattern = { [".*/queries/.*%.scm"] = "query" } })
 
 --------------------------
 -- lazy.nvim configuration
