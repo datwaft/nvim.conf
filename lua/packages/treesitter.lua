6dcaacbb77ab75b0fd1ddec54fdb2d0996a61714
datwaft <datwaftx@gmail.com>
2024-03-25 01:01:55 -0600

    feat(tree-sitter): add corpus parser

--- lua/packages/treesitter.lua
+++ lua/packages/treesitter.lua
@@ -1,38 +1,45 @@
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-refactor",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "nvim-treesitter/nvim-treesitter-context",
-    "andymass/vim-matchup",
-  },
-  build = ":TSUpdate",
-  main = "nvim-treesitter.configs",
-  init = function()
-    vim.opt.foldmethod = "expr"
-    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-  end,
-  opts = {
-    ensure_installed = "all",
-    highlight = { enable = true, disable = { "latex", "tmux" } },
-    indent = { enable = true },
-    format = { enable = true },
-    matchup = { enable = true },
-    refactor = {
-      smart_rename = { enable = true, keymaps = { smart_rename = "<localleader>rn" } },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-refactor",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-context",
+      "andymass/vim-matchup",
     },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["as"] = "@statement.outer",
-          ["aS"] = "@statement.top",
-          ["ic"] = "@cell.inner",
-          ["ac"] = "@cell.outer",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    init = function()
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    end,
+    opts = {
+      ensure_installed = "all",
+      highlight = { enable = true, disable = { "latex", "tmux" } },
+      indent = { enable = true },
+      format = { enable = true },
+      matchup = { enable = true },
+      refactor = {
+        smart_rename = { enable = true, keymaps = { smart_rename = "<localleader>rn" } },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["as"] = "@statement.outer",
+            ["aS"] = "@statement.top",
+            ["ic"] = "@cell.inner",
+            ["ac"] = "@cell.outer",
+          },
         },
       },
     },
+  },
+  {
+    "datwaft/tree-sitter-corpus",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    build = ":TSUpdate corpus",
   },
 }
