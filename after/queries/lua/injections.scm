172af35f45393f66266748cd92ffe6073cd432a5
datwaft <datwaftx@gmail.com>
2024-03-25 16:00:35 -0600

    feat(queries): remove vim.filetype.add `luap` injection because it has been merged into upstream
    See https://github.com/nvim-treesitter/nvim-treesitter/pull/6324 for more information

--- after/queries/lua/injections.scm
+++ after/queries/lua/injections.scm
@@ -1,23 +1,5 @@
 ;; extends
 
-;; vim.filetype.add({ pattern = { ["luap"] = "filetype" } })
-((function_call
-  name: (_) @_filetypeadd_identifier
-  arguments:
-    (arguments
-      (table_constructor
-        (field
-          name: (_) @_pattern_key
-          value:
-            (table_constructor
-              (field
-                name:
-                  (string
-                    content: _ @injection.content)))))))
-  (#set! injection.language "luap")
-  (#eq? @_filetypeadd_identifier "vim.filetype.add")
-  (#eq? @_pattern_key "pattern"))
-
 ;; vim.opt.grepprg = "bash"
 ((assignment_statement
    (variable_list) @_variable_symbol
