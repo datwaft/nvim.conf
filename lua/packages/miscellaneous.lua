8f259a830bec767593194c21223b8de20a0327a5
datwaft <datwaftx@gmail.com>
2024-03-28 15:40:03 -0600

    revert: "feat: add spellfile.nvim to install spell files without netrw"
    This reverts commit c7504546bbf5d86eae2d15ba295a3cf1c0ee80d8.

--- lua/packages/miscellaneous.lua
+++ lua/packages/miscellaneous.lua
@@ -8,8 +8,6 @@ return {
   { "sQVe/sort.nvim", config = true },
   -- Sync spell files
   "micarmst/vim-spellsync",
-  -- Install spell files without netrw
-  "cuducos/spellfile.nvim",
   -- Add programming dictionary
   {
     "psliwka/vim-dirtytalk",
