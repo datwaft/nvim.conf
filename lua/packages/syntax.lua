4a8f40bba70e194f6886aaf96ebc953763c13af4
datwaft <datwaftx@gmail.com>
2024-04-01 14:36:29 -0600

    revert: "feat: remove vimtex package"
    This reverts commit 985bcdb06b6178f7af2749acfeaea1f108d5a546.

--- lua/packages/syntax.lua
+++ lua/packages/syntax.lua
@@ -2,6 +2,23 @@
 return {
   -- Fennel
   "jaawerth/fennel.vim",
+  -- LaTeX
+  {
+    "lervag/vimtex",
+    init = function()
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_compiler_latexmk = {
+        options = {
+          "-shell-escape",
+          "-verbose",
+          "-file-line-error",
+          "-synctex=1",
+          "-interaction=nonstopmode",
+        },
+      }
+      vim.g.vimtex_syntax_conceal_disable = 1
+    end,
+  },
   -- Ansible
   "mfussenegger/nvim-ansible",
   -- ARM ASM
