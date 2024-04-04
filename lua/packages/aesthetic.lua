784b133e8b472989741e2b090b53f1fffa8c1492
datwaft <datwaftx@gmail.com>
2024-04-04 00:10:24 -0600

    feat(lualine): configure icons

--- lua/packages/aesthetic.lua
+++ lua/packages/aesthetic.lua
@@ -11,9 +11,34 @@ return {
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype", "PencilMode" },
+        lualine_b = {
+          "branch",
+          {
+            "diagnostics",
+            symbols = {
+              error = " ",
+              warn = " ",
+              info = " ",
+              hint = " ",
+            },
+          },
+        },
+        lualine_c = {
+          "filename",
+          {
+            "diff",
+            symbols = {
+              added = " ",
+              modified = " ",
+              removed = " ",
+            },
+          },
+        },
+        lualine_x = {
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
