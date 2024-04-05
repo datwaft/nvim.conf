fb48e707b88c9adf23528eb859e675d8771cbc20
datwaft <datwaftx@gmail.com>
2024-04-05 15:35:15 -0600

    feat: define command for toggling breakpoints

--- lua/packages/debugging.lua
+++ lua/packages/debugging.lua
@@ -40,5 +40,13 @@ return {
     define_sign("DapLogPoint", icons.debug.log)
     define_sign("DapStopped", icons.debug.stopped)
     define_sign("DapBreakpointRejected", icons.debug.rejected)
+    -- Define command for toggling breakpoints
+    vim.api.nvim_create_user_command("Breakpoint", function(opts)
+      if opts.bang then
+        dap.toggle_breakpoint(nil, nil, opts.fargs[1])
+      else
+        dap.set_breakpoint(nil, nil, opts.fargs[1])
+      end
+    end, { nargs = "?", bang = true, desc = "Set or toggle a breakpoint" })
   end,
 }
