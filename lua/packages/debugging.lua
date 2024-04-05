7e804b5c44bf8f67f6d29b2ddcf6925a731ff6c5
datwaft <datwaftx@gmail.com>
2024-04-05 15:59:37 -0600

    feat: add dap ui

--- lua/packages/debugging.lua
+++ lua/packages/debugging.lua
@@ -25,28 +25,56 @@ configs.lldb = {
 
 ---@type LazySpec
 return {
-  "mfussenegger/nvim-dap",
-  config = function()
-    local dap = require("dap")
-    -- Configure adapters
-    dap.adapters = adapters
-    -- Configure configurations
-    dap.configurations.c = configs.lldb
-    dap.configurations.cpp = configs.lldb
-    dap.configurations.rust = configs.lldb
-    -- Define signs
-    define_sign("DapBreakpoint", icons.debug.breakpoint)
-    define_sign("DapBreakpointCondition", icons.debug.condition)
-    define_sign("DapLogPoint", icons.debug.log)
-    define_sign("DapStopped", icons.debug.stopped)
-    define_sign("DapBreakpointRejected", icons.debug.rejected)
-    -- Define command for toggling breakpoints
-    vim.api.nvim_create_user_command("Breakpoint", function(opts)
-      if opts.bang then
-        dap.toggle_breakpoint(nil, nil, opts.fargs[1])
-      else
-        dap.set_breakpoint(nil, nil, opts.fargs[1])
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      -- Configure adapters
+      dap.adapters = adapters
+      -- Configure configurations
+      dap.configurations.c = configs.lldb
+      dap.configurations.cpp = configs.lldb
+      dap.configurations.rust = configs.lldb
+      -- Define signs
+      define_sign("DapBreakpoint", icons.debug.breakpoint)
+      define_sign("DapBreakpointCondition", icons.debug.condition)
+      define_sign("DapLogPoint", icons.debug.log)
+      define_sign("DapStopped", icons.debug.stopped)
+      define_sign("DapBreakpointRejected", icons.debug.rejected)
+      -- Define command for toggling breakpoints
+      vim.api.nvim_create_user_command("Breakpoint", function(opts)
+        if opts.bang then
+          dap.toggle_breakpoint(nil, nil, opts.fargs[1])
+        else
+          dap.set_breakpoint(nil, nil, opts.fargs[1])
+        end
+      end, { nargs = "?", bang = true, desc = "Set or toggle a breakpoint" })
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui = function()
+        dapui.open()
       end
-    end, { nargs = "?", bang = true, desc = "Set or toggle a breakpoint" })
-  end,
+      dap.listeners.before.launch.dapui = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui = function()
+        dapui.close()
+      end
+    end,
+  },
 }
