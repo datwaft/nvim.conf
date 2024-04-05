5f3fb88d2ac8e4255100f87af1b5ed07989fe20f
datwaft <datwaftx@gmail.com>
2024-04-05 14:47:13 -0600

    feat: define signs for breakpoints

--- lua/packages/debugging.lua
+++ lua/packages/debugging.lua
@@ -28,9 +28,17 @@ return {
   "mfussenegger/nvim-dap",
   config = function()
     local dap = require("dap")
+    -- Configure adapters
     dap.adapters = adapters
+    -- Configure configurations
     dap.configurations.c = configs.lldb
     dap.configurations.cpp = configs.lldb
     dap.configurations.rust = configs.lldb
+    -- Define signs
+    vim.fn.sign_define("DapBreakpoint", { text = icons.debug.breakpoint, texthl = "DapBreakpoint" })
+    vim.fn.sign_define("DapBreakpointCondition", { text = icons.debug.condition, texthl = "DapBreakpointCondition" })
+    vim.fn.sign_define("DapLogPoint", { text = icons.debug.log, texthl = "DapLogPoint" })
+    vim.fn.sign_define("DapStopped", { text = icons.debug.stopped, texthl = "DapStopped" })
+    vim.fn.sign_define("DapBreakpointRejected", { text = icons.debug.rejected, texthl = "DapBreakpointRejected" })
   end,
 }
