239f8bf25934e9b6d9e79f4efd45afa67b99a5ff
datwaft <datwaftx@gmail.com>
2024-04-09 17:21:43 -0600

    style: apply stylua

--- lua/packages/aesthetic.lua
+++ lua/packages/aesthetic.lua
@@ -3,7 +3,12 @@ return {
   -- Enhance vim.ui.select & vim.ui.input
   { "stevearc/dressing.nvim", event = "VeryLazy", opts = { input = { enabled = false } } },
   -- Preview ranges
-  { "winston0410/range-highlight.nvim", event = "VeryLazy", dependencies = { "winston0410/cmd-parser.nvim" }, config = true },
+  {
+    "winston0410/range-highlight.nvim",
+    event = "VeryLazy",
+    dependencies = { "winston0410/cmd-parser.nvim" },
+    config = true,
+  },
   -- Highlight color codes
   {
     "NvChad/nvim-colorizer.lua",
