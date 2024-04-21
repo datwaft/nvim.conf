0caae2e3eb468e2fbfd96aecdb8204371a2ecfc1
datwaft <datwaftx@gmail.com>
2024-04-20 22:30:33 -0600

    feat: remove noice.nvim

--- lua/packages/aesthetic.lua
+++ lua/packages/aesthetic.lua
@@ -19,28 +19,4 @@ return {
   },
   -- Indentation lines
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", event = "VeryLazy", opts = { scope = { enabled = false } } },
-  -- Experimental UI enhancements
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {
-      lsp = {
-        progress = { enabled = false },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      presets = {
-        inc_rename = false,
-        lsp_doc_border = false,
-      },
-      cmdline = { enabled = false },
-      messages = { enabled = false },
-      popupmenu = { enabled = false },
-      notify = { enabled = false },
-    },
-  },
 }
