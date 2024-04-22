8ca9c6308a9fe9c00298080f5250bfb7e00e4e61
datwaft <datwaftx@gmail.com>
2024-04-21 18:55:13 -0600

    feat: remove range-highlight
    It is buggy

--- lua/packages/aesthetic.lua
+++ lua/packages/aesthetic.lua
@@ -2,13 +2,6 @@
 return {
   -- Enhance vim.ui.select & vim.ui.input
   { "stevearc/dressing.nvim", event = "VeryLazy", opts = { input = { enabled = false } } },
-  -- Preview ranges
-  {
-    "winston0410/range-highlight.nvim",
-    event = "VeryLazy",
-    dependencies = { "winston0410/cmd-parser.nvim" },
-    config = true,
-  },
   -- Highlight color codes
   {
     "brenoprata10/nvim-highlight-colors",
