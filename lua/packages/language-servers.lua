a04c8effe93ad326934fc9dc9d3e6b9c2517951f
datwaft <datwaftx@gmail.com>
2024-04-24 09:53:59 -0600

    feat(lsp): add inlay hints to javascript too

--- lua/packages/language-servers.lua
+++ lua/packages/language-servers.lua
@@ -67,10 +67,18 @@ local function config()
   require("lspconfig.configs").vtsls = require("vtsls").lspconfig
   lsp.vtsls.setup({
     settings = {
+      javascript = {
+        inlayHints = {
+          parameterNames = { enabled = "literals" },
+          variableTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+        },
+      },
       typescript = {
         inlayHints = {
           parameterNames = { enabled = "literals" },
-          parameterTypes = { enabled = true },
           variableTypes = { enabled = true },
           propertyDeclarationTypes = { enabled = true },
           functionLikeReturnTypes = { enabled = true },
