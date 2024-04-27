b1803b0903660df558e25d5bcd83950b12c56953
datwaft <datwaftx@gmail.com>
2024-04-27 12:29:38 -0600

    feat(fish): inject fish into bind strings

---
+++ after/queries/fish/injections.scm
@@ -0,0 +1,9 @@
+;; extends
+
+;; bind \cf 'something'
+((command
+   name: (word) @command_name
+   argument: (single_quote_string) @injection.content)
+ (#offset! @injection.content 0 1 0 -1)
+ (#set! injection.language "fish")
+ (#eq? @command_name "bind"))
