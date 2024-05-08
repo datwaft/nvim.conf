e4262645796991974833d6aba98799ed50c447af
datwaft <datwaftx@gmail.com>
2024-05-07 22:40:38 -0600

    feat: add border to :LspInfo

--- lua/packages/language-servers.lua
+++ lua/packages/language-servers.lua
@@ -3,6 +3,9 @@
 local function config()
   local lsp = require("lspconfig")
 
+  -- Add borders to LSP windows
+  require("lspconfig.ui.windows").default_options.border = border
+
   local capabilities = require("cmp_nvim_lsp").default_capabilities()
   local handlers = {
     ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
