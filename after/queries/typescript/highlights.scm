f6331df0685245eeff6b723eda8bc1a5b7895fb6
datwaft <datwaftx@gmail.com>
2024-05-24 10:12:05 -0600

    feat(queries): spell-check Playwright's describe descriptions

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -9,3 +9,15 @@
     (string (string_fragment) @spell))
   (#eq? @identifier "test"))
 
+;; Playwright's `test.describe("something", async () => {})`
+(call_expression
+  function:
+    (member_expression
+      object: (identifier) @identifier
+      property: (property_identifier) @property_identifier)
+  arguments:
+    (arguments
+      .
+      (string (string_fragment) @spell))
+  (#eq? @identifier "test")
+  (#eq? @property_identifier "describe"))
