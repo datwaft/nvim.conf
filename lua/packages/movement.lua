9efb368ccc8dd3acffd8284a344fbc19a0bfeabb
datwaft <datwaftx@gmail.com>
2024-06-04 09:02:43 -0600

    feat: un-pin `nvim-spider`

--- lua/packages/movement.lua
+++ lua/packages/movement.lua
@@ -11,7 +11,6 @@ return {
   },
   {
     "chrisgrieser/nvim-spider",
-    commit = "b2f2af0563bb631864cab262d483118f3610e662", -- TODO: check in the future to see if issue is still present
     keys = {
       { mode = { "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<cr>", desc = "spider-w" },
       { mode = { "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<cr>", desc = "spider-e" },
