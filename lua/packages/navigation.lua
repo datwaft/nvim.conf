22c05f26a0fe7a1d35f017469d1ebaab4292932f
datwaft <datwaftx@gmail.com>
2024-06-10 18:50:06 -0600

    feat: use fzf-lua instead of telescope

--- lua/packages/navigation.lua
+++ lua/packages/navigation.lua
@@ -2,75 +2,16 @@
 return {
   -- Fuzzy-finding
   {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      { "nvim-telescope/telescope-smart-history.nvim", dependencies = { "tami5/sqlite.lua" } },
-      "nvim-tree/nvim-web-devicons",
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { [[<C-\>]], "<cmd>lua require('fzf-lua').buffers()<cr>" },
+      { [[<C-p>]], "<cmd>lua require('fzf-lua').files()<cr>" },
+      { [[<C-g>]], "<cmd>lua require('fzf-lua').grep()<cr>" },
+      { [[<C-l>]], "<cmd>lua require('fzf-lua').live_grep()<cr>" },
+      { [[<C-k>]], "<cmd>lua require('fzf-lua').commands()<cr>" },
+      { [[<F1>]], "<cmd>lua require('fzf-lua').helptags()<cr>" },
     },
-    config = function(_, opts)
-      require("telescope").setup(opts)
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("smart_history")
-    end,
-    keys = function()
-      local builtin = require("telescope.builtin")
-      return {
-        { "<leader>ff", builtin.find_files },
-        { "<leader>fg", builtin.live_grep },
-        { "<leader>fb", builtin.buffers },
-        { "<leader>fh", builtin.help_tags },
-        { "<leader>fq", builtin.quickfix },
-        { "<leader>fl", builtin.loclist },
-        { "z=", builtin.spell_suggest },
-      }
-    end,
-    opts = function()
-      local actions = require("telescope.actions")
-      return {
-        defaults = {
-          mappings = {
-            i = {
-              ["<C-h>"] = actions.which_key,
-              ["<Esc>"] = actions.close,
-              ["<C-q>"] = actions.smart_send_to_qflist,
-              ["<C-k>"] = actions.smart_send_to_loclist,
-              ["<M-Up>"] = actions.cycle_history_prev,
-              ["<M-Down>"] = actions.cycle_history_next,
-            },
-          },
-          history = {
-            path = vim.fn.stdpath("data") .. "/databases/telescope_history.sqlite3",
-            limit = 100,
-          },
-          prompt_prefix = "   ",
-          selection_caret = "  ",
-          layout_config = {
-            horizontal = {
-              prompt_position = "top",
-              preview_width = 0.55,
-              results_width = 0.8,
-            },
-            vertical = { mirror = false },
-            width = 0.87,
-            height = 0.80,
-            preview_cutoff = 120,
-          },
-          sorting_strategy = "ascending",
-        },
-      }
-    end,
-    init = function()
-      vim.api.nvim_create_autocmd("VimEnter", {
-        group = vim.api.nvim_create_augroup("mkdir-databases", { clear = true }),
-        callback = function()
-          vim.fn.mkdir(vim.fn.stdpath("data") .. "/databases", "p")
-        end,
-        once = true,
-      })
-    end,
   },
   -- File manipulation
   {
