32f4a2f3fab12404deac6ea4825975b4a7d7f7de
datwaft <datwaftx@gmail.com>
2024-06-10 19:09:56 -0600

    feat: install neogit

--- lua/packages/git.lua
+++ lua/packages/git.lua
@@ -4,4 +4,13 @@ return {
     "echasnovski/mini.diff",
     config = true,
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    config = true,
+  },
 }
