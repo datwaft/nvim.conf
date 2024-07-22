ccc3e4285217c39daa5de6719ae482daa0b3b25c
datwaft <datwaftx@gmail.com>
2024-07-21 20:56:00 -0600

    feat: make `vim-dadbod-ui` not lazy

--- lua/packages/integration.lua
+++ lua/packages/integration.lua
@@ -52,15 +52,10 @@ return {
   -- Database
   {
     "kristijanhusak/vim-dadbod-ui",
+    lazy = false,
     dependencies = {
       { "tpope/vim-dadbod" },
       { "kristijanhusak/vim-dadbod-completion" },
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
     },
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
