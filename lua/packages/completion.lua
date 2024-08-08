23d208b6c74f2145dd686204bdb97b4f0056d82c
datwaft <datwaftx@gmail.com>
2024-08-08 11:02:55 -0600

    feat(snippets): add playwright snippets

--- lua/packages/completion.lua
+++ lua/packages/completion.lua
@@ -183,6 +183,7 @@ return {
     build = "make install_jsregexp",
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
+      require("luasnip.loaders.from_snipmate").lazy_load()
       require("luasnip.loaders.from_vscode").lazy_load()
     end,
   },
