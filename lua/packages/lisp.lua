68f8d6d3d2101858e2a46edfa45d44e5b7b1b9e8
datwaft <datwaftx@gmail.com>
2024-08-15 22:13:56 -0600

    feat: disable conjure `doc_word` mapping

--- lua/packages/lisp.lua
+++ lua/packages/lisp.lua
@@ -5,6 +5,9 @@ return {
     "Olical/conjure",
     ft = { "clojure", "fennel" },
     lazy = true,
+    init = function()
+      vim.g["conjure#mapping#doc_word"] = false
+    end,
   },
   -- Parinfer
   { "gpanders/nvim-parinfer" },
