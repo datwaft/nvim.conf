6b94e587c53f6422fd52e79eb4a85aa5611aa75b
datwaft <datwaftx@gmail.com>
2024-09-20 00:08:24 -0600

    feat: remove tiny-devicons-auto-colors.nvim

--- lua/packages/aesthetic.lua
+++ lua/packages/aesthetic.lua
@@ -11,21 +11,6 @@ return {
   },
   -- Indentation lines
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", event = "VeryLazy", opts = { scope = { enabled = false } } },
-  -- Colorscheme colors in devicons
-  {
-    "rachartier/tiny-devicons-auto-colors.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      { "catppuccin/nvim", name = "catppuccin" },
-    },
-    event = "VeryLazy",
-    opts = function()
-      local theme_colors = require("catppuccin.palettes").get_palette("mocha")
-      return {
-        colors = theme_colors,
-      }
-    end,
-  },
   -- Highlight NOTE and the like
   {
     "folke/todo-comments.nvim",
