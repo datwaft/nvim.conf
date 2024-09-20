dcf2fda3aab82a3bf9bab9d2624470d6bcf84f1c
datwaft <datwaftx@gmail.com>
2024-09-20 14:43:42 -0600

    revert: "feat: remove tiny-devicons-auto-colors.nvim"
    This reverts commit 6b94e587c53f6422fd52e79eb4a85aa5611aa75b.

--- lua/packages/aesthetic.lua
+++ lua/packages/aesthetic.lua
@@ -11,6 +11,21 @@ return {
   },
   -- Indentation lines
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", event = "VeryLazy", opts = { scope = { enabled = false } } },
+  -- Colorscheme colors in devicons
+  {
+    "rachartier/tiny-devicons-auto-colors.nvim",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons", config = true },
+      { "catppuccin/nvim", name = "catppuccin" },
+    },
+    event = "VeryLazy",
+    opts = function()
+      local theme_colors = require("catppuccin.palettes").get_palette("mocha")
+      return {
+        colors = theme_colors,
+      }
+    end,
+  },
   -- Highlight NOTE and the like
   {
     "folke/todo-comments.nvim",
