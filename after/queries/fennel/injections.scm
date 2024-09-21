e8793269aadf3291524715d3fcc9175666c63a4b
datwaft <datwaftx@gmail.com>
2024-09-21 11:34:24 -0600

    feat(fennel): support `vim.filetype.add` injection

--- after/queries/fennel/injections.scm
+++ after/queries/fennel/injections.scm
@@ -1,5 +1,23 @@
 ;; extends
 
+;; (vim.filetype.add {:pattern {"<luap injection>" "filetype"}})
+(list
+  call: (multi_symbol
+          base: (symbol_fragment) @_vim
+          (#eq? @_vim "vim")
+          member: (symbol_fragment) @_filetype
+          (#eq? @_filetype "filetype")
+          member: (symbol_fragment) @_add
+          (#eq? @_add "add"))
+  item: (table
+          (table_pair
+            key: (string (string_content) @_key)
+            (#eq? @_key "pattern")
+            value: (table
+                     (table_pair
+                       key: (string (string_content) @injection.content)
+                       (#set! injection.language "luap"))))))
+
 ;; (set! grepprg "bash")
 (list
   .
