e847891afff0e3344a1e3ca472b4e02ef2ffa0af
datwaft <datwaftx@gmail.com>
2024-10-01 08:58:43 -0600

    feat(lsp): remove fastaction.nvim

--- lua/packages/language-servers.lua
+++ lua/packages/language-servers.lua
@@ -212,17 +212,6 @@ return {
       return { { "gx", require("lsplinks").gx } }
     end,
   },
-  -- Enhance code actions select
-  {
-    "Chaitanyabsprip/fastaction.nvim",
-    ---@module "fastaction"
-    ---@type FastActionConfig
-    opts = {
-      popup = {
-        border = border,
-      },
-    },
-  },
   -- LSP in embedded code
   {
     "jmbuhr/otter.nvim",
