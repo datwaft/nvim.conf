fbf5c5988eb7c69e4e8816099d23c8989bef1823
datwaft <datwaftx@gmail.com>
2024-10-09 20:46:00 -0600

    feat: add command to create new notebook

---
+++ plugin/notebook.lua
@@ -0,0 +1,51 @@
+local default_notebook = [[
+  {
+    "cells": [
+     {
+      "cell_type": "markdown",
+      "metadata": {},
+      "source": [
+        ""
+      ]
+     }
+    ],
+    "metadata": {
+     "kernelspec": {
+      "display_name": "Python 3",
+      "language": "python",
+      "name": "python3"
+     },
+     "language_info": {
+      "codemirror_mode": {
+        "name": "ipython"
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3"
+     }
+    },
+    "nbformat": 4,
+    "nbformat_minor": 5
+  }
+]]
+
+local function new_notebook(filename)
+  local path = filename .. ".ipynb"
+  local file = io.open(path, "w")
+  if file then
+    file:write(default_notebook)
+    file:close()
+    vim.cmd.edit(path)
+  else
+    print("Error: Could not open new notebook file for writing.")
+  end
+end
+
+vim.api.nvim_create_user_command("NewNotebook", function(opts)
+  new_notebook(opts.args)
+end, {
+  nargs = 1,
+  complete = "file",
+})
