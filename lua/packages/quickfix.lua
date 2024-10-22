2f6ca7c1f519e36aed9362fd8c49d0944d320be5
david-guevara-gorillalogic <david.guevara@gorillalogic.com>
2024-10-22 08:36:24 -0600

    feat(quicker): do not load buffers for highlighting

--- lua/packages/quickfix.lua
+++ lua/packages/quickfix.lua
@@ -9,6 +9,9 @@ return {
       opts = {
         winfixbuf = true,
       },
+      highlight = {
+        load_buffers = false,
+      },
       keys = {
         { ">", [[<cmd>lua require("quicker").expand()<cr>]] },
         { "<", [[<cmd>lua require("quicker").collapse()<cr>]] },
