dfae03c3a3a430f92b4e85e5954d84aadb7a2977
datwaft <datwaftx@gmail.com>
2024-11-08 13:07:43 -0600

    feat: remove neogit

--- lua/packages/git.lua
+++ lua/packages/git.lua
@@ -36,13 +36,4 @@ return {
       end,
     },
   },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "ibhagwan/fzf-lua",
-    },
-    config = true,
-  },
 }
