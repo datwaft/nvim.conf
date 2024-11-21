67c0019468646d45cb261657dfa0cd5848f1d1e9
david-guevara-gorillalogic <david.guevara@gorillalogic.com>
2024-11-21 08:47:18 -0600

    feat(queries): support for `test.anything("@spell @string.documentation")`

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -13,6 +13,20 @@
         (string_fragment) @spell) @string.documentation
     ]))
 
+; test.something("@spell @string.documentation")
+(call_expression
+  function: (member_expression
+              object: (identifier) @_test)
+  (#eq? @_test "test")
+  arguments: (arguments
+    .
+    [
+      (string
+        (string_fragment) @spell) @string.documentation
+      (template_string
+        (string_fragment) @spell) @string.documentation
+    ]))
+
 ; test.describe("@spell @string.documentation")
 (call_expression
   function: (member_expression
