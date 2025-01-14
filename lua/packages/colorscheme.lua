53759c76de348ff9d2df40b30dc8608c9e9870c4
david-guevara-gorillalogic <david.guevara@gorillalogic.com>
2025-01-14 11:12:05 -0600

    feat(colorscheme): dim rainbow delimiter colors

--- lua/packages/colorscheme.lua
+++ lua/packages/colorscheme.lua
@@ -1,3 +1,24 @@
+---@param color string e.g. "#12fa56"
+---@param factor number e.g. 0.7
+local function dim_color(color, factor)
+  -- We first extract the number as an `integer`
+  ---@type unknown, unknown, string
+  local _, _, hex = color:find("^#(%w+)$")
+  local number = tonumber(hex, 16)
+  -- Then we extract the RGB components
+  local r = bit.rshift(bit.band(number, 0xFF0000), 16)
+  local g = bit.rshift(bit.band(number, 0x00FF00), 8)
+  local b = bit.band(number, 0x0000FF)
+  -- We then dim each color by a factor
+  r = math.floor(r * factor)
+  g = math.floor(g * factor)
+  b = math.floor(b * factor)
+  -- We then convert those RGB components into an `integer` again
+  local dimmed_color = bit.lshift(r, 16) + bit.lshift(g, 8) + b
+  -- And finally we return the `integer` converted into a hex string
+  return ("#%06x"):format(dimmed_color)
+end
+
 ---@type LazySpec
 return {
   "catppuccin/nvim",
@@ -20,6 +41,13 @@ return {
         DiagnosticVirtualTextOk = { style = {} },
         FzfLuaBorder = { fg = colors.blue },
         QuickFixLineNr = { fg = colors.peach },
+        RainbowDelimiterRed = { fg = dim_color(colors.red, 0.5), style = { "bold" } },
+        RainbowDelimiterYellow = { fg = dim_color(colors.yellow, 0.5), style = { "bold" } },
+        RainbowDelimiterBlue = { fg = dim_color(colors.blue, 0.5), style = { "bold" } },
+        RainbowDelimiterOrange = { fg = dim_color(colors.peach, 0.5), style = { "bold" } },
+        RainbowDelimiterGreen = { fg = dim_color(colors.green, 0.5), style = { "bold" } },
+        RainbowDelimiterViolet = { fg = dim_color(colors.mauve, 0.5), style = { "bold" } },
+        RainbowDelimiterCyan = { fg = dim_color(colors.teal, 0.5), style = { "bold" } },
       }
     end,
     transparent_background = true,
