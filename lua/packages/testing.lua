7961f2676f87f28f61bc8cf64312254e612254bc
datwaft <datwaftx@gmail.com>
2025-02-22 14:40:12 -0600

    feat(testing): add neotest with playwright support

---
+++ lua/packages/testing.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    -- Adapters
+    "thenbe/neotest-playwright",
+    -- Dependencies
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  lazy = false,
+  opts = function()
+    return {
+      adapters = {
+        require("neotest-playwright").adapter({
+          options = {
+            persist_project_selection = true,
+            enable_dynamic_test_discovery = true,
+          },
+        }),
+      },
+      consumers = {
+        playwright = require("neotest-playwright.consumers").consumers,
+      },
+    }
+  end,
+  keys = function()
+    return {
+      {
+        "got",
+        ---@diagnostic disable-next-line: undefined-field
+        function() require("neotest").playwright.attachment() end,
+      },
+      { "gor", function() require("neotest").run.run() end },
+      { "goR", function() require("neotest").run.run(vim.fn.expand("%")) end },
+      { "gos", function() require("neotest").run.stop() end },
+      { "goa", function() require("neotest").run.attach() end },
+      { "<C-w>o", function() require("neotest").output.open({ enter = true }) end },
+      { "<C-w>O", function() require("neotest").summary.toggle() end },
+    }
+  end,
+}
