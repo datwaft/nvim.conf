23b9cd960a17bbe1eaf056415952fe7e4817f818
david-guevara-gorillalogic <david.guevara@gorillalogic.com>
2025-05-30 08:43:59 -0600

    refactor(treesitter): use `vim.treesitter.query.get()`

--- lua/packages/treesitter.lua
+++ lua/packages/treesitter.lua
@@ -1,19 +1,3 @@
----@type table<string, table<string, true>>
-local get_features_cache = {}
---- Lists all the features of a tree-sitter parser
----@param language string
----@return table<string, true>
-local function get_features(language)
-  if get_features_cache[language] then return get_features_cache[language] end
-  ---@type table<string, true>
-  local features = {}
-  for _, path in ipairs(vim.api.nvim_get_runtime_file(("queries/%s/*.scm"):format(language), true)) do
-    features[vim.fn.fnamemodify(path, ":t:r")] = true
-  end
-  get_features_cache[language] = features
-  return features
-end
-
 ---@type LazySpec
 return {
   {
@@ -28,13 +12,16 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         group = vim.api.nvim_create_augroup("tree-sitter-enable", { clear = true }),
         callback = function(args)
-          local bufnr = args.buf
-          local language = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype)
-          if not language or language == "tmux" then return end
-          local features = get_features(language)
-          if features["highlights"] then vim.treesitter.start(bufnr) end
-          if features["indents"] then vim.opt_local.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()' end
-          if features["folds"] then
+          local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+          if not lang then return end
+
+          if vim.treesitter.query.get(lang, "highlights") then vim.treesitter.start(args.buf) end
+
+          if vim.treesitter.query.get(lang, "indents") then
+            vim.opt_local.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()'
+          end
+
+          if vim.treesitter.query.get(lang, "folds") then
             vim.opt_local.foldmethod = "expr"
             vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
           end
