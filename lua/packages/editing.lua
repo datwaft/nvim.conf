e0bc4095139b7ed910edc23bd379780bdad0ceeb
david-guevara-gorillalogic <david.guevara@gorillalogic.com>
2025-07-10 08:50:12 -0600

    feat: add `dial.nvim` plugin

--- lua/packages/editing.lua
+++ lua/packages/editing.lua
@@ -150,4 +150,18 @@ return {
       vim.keymap.set("x", "A", mc.appendVisual)
     end,
   },
+  -- Increment/decrement
+  {
+    "monaqa/dial.nvim",
+    config = function()
+      vim.keymap.set("n", "<C-a>", function() require("dial.map").manipulate("increment", "normal") end)
+      vim.keymap.set("n", "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end)
+      vim.keymap.set("n", "g<C-a>", function() require("dial.map").manipulate("increment", "gnormal") end)
+      vim.keymap.set("n", "g<C-x>", function() require("dial.map").manipulate("decrement", "gnormal") end)
+      vim.keymap.set("v", "<C-a>", function() require("dial.map").manipulate("increment", "visual") end)
+      vim.keymap.set("v", "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end)
+      vim.keymap.set("v", "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end)
+      vim.keymap.set("v", "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end)
+    end,
+  },
 }
