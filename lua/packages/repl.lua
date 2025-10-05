d323bd14897b719c085f74e349208e636b0b99bf
datwaft <datwaftx@gmail.com>
2025-10-05 01:15:47 -0600

    feat(yarepl.nvim): add configuration options

--- lua/packages/repl.lua
+++ lua/packages/repl.lua
@@ -2,7 +2,13 @@
 return {
   {
     "milanglacier/yarepl.nvim",
-    opts = {},
+    opts = {
+      metas = {
+        ipython = {
+          cmd = function() return vim.b.repl_ipython_cmd or vim.g.repl_ipython_cmd or "ipython" end,
+        },
+      },
+    },
     init = function()
       vim.api.nvim_create_autocmd("FileType", {
         group = vim.api.nvim_create_augroup("yarepl-keybinds", { clear = true }),
@@ -12,6 +18,16 @@ return {
             vim.keymap.set("n", "<C-s>", "<Plug>(REPLSendOperator)", { buffer = true })
             vim.keymap.set("n", "<C-s>s", "<Plug>(REPLSendLine)", { buffer = true })
             vim.keymap.set("n", "<C-s><C-s>", "<Plug>(REPLSendLine)", { buffer = true })
+            if vim.b.repl_default or vim.g.repl_default then
+              vim.keymap.set(
+                "n",
+                "<C-s>I",
+                ("<Plug>(REPLStart-%s)"):format(vim.b.repl_default or vim.g.repl_default),
+                { buffer = true }
+              )
+            else
+              vim.keymap.set("n", "<C-s>I", "<Plug>(REPLStart)", { buffer = true })
+            end
           end
         end,
       })
