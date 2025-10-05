b2685672d04ed37a0e1265f039925ecb69a85299
datwaft <datwaftx@gmail.com>
2025-10-05 00:38:16 -0600

    feat: add yarepl.nvim

--- lua/packages/repl.lua
+++ lua/packages/repl.lua
@@ -1,27 +1,17 @@
 ---@type LazySpec
 return {
   {
-    "jpalardy/vim-slime",
-    event = "VeryLazy",
+    "milanglacier/yarepl.nvim",
+    opts = {},
     init = function()
-      vim.g.slime_target = "tmux"
-      vim.g.slime_default_config = {
-        socket_name = vim.fn.split(vim.env.TMUX, ",")[1],
-        target_pane = "{last}",
-      }
-      vim.g.slime_dont_ask_default = 1
-      vim.g.slime_bracketed_paste = 1
-      vim.g.slime_no_mappings = 1
-
       vim.api.nvim_create_autocmd("FileType", {
-        group = vim.api.nvim_create_augroup("vim-slime-keybinds", { clear = true }),
+        group = vim.api.nvim_create_augroup("yarepl-keybinds", { clear = true }),
         callback = function()
           if vim.bo.buftype ~= "nofile" then
-            vim.keymap.set("x", "<C-s>", "<Plug>SlimeRegionSend", { buffer = true })
-            vim.keymap.set("n", "<C-s>", "<Plug>SlimeMotionSend", { buffer = true })
-            vim.keymap.set("n", "<C-s>s", "<Plug>SlimeLineSend", { buffer = true })
-            vim.keymap.set("n", "<C-s><C-s>", "<Plug>SlimeLineSend", { buffer = true })
-            vim.keymap.set("n", "<C-s>c", "<Plug>SlimeConfig", { buffer = true })
+            vim.keymap.set("x", "<C-s>", "<Plug>(REPLSendVisual)", { buffer = true })
+            vim.keymap.set("n", "<C-s>", "<Plug>(REPLSendOperator)", { buffer = true })
+            vim.keymap.set("n", "<C-s>s", "<Plug>(REPLSendLine)", { buffer = true })
+            vim.keymap.set("n", "<C-s><C-s>", "<Plug>(REPLSendLine)", { buffer = true })
           end
         end,
       })
