feab6918a98a734e2dbe3aa70341a6f278ccd6a9
datwaft <datwaftx@gmail.com>
2025-12-17 13:15:06 -0600

    feat: install amp.nvim
    Amp CLI integration

--- lua/packages/assist.lua
+++ lua/packages/assist.lua
@@ -1,47 +1,56 @@
 ---@type LazySpec
 return {
-  "folke/sidekick.nvim",
-  lazy = false,
-  ---@type sidekick.Config
-  opts = {
-    nes = {
-      enabled = true,
+  {
+    "folke/sidekick.nvim",
+    lazy = false,
+    ---@type sidekick.Config
+    opts = {
+      nes = {
+        enabled = true,
+      },
     },
+    keys = {
+      {
+        "<M-l>",
+        function()
+          if require("sidekick").nes_jump_or_apply() then return end
+          if vim.lsp.inline_completion.get() then return end
+          return "<M-l>"
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+        mode = { "i", "n" },
+      },
+      {
+        "<M-Right>",
+        function()
+          if require("sidekick").nes_jump_or_apply() then return end
+          if vim.lsp.inline_completion.get() then return end
+          return "<M-Right>"
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+        mode = { "i", "n" },
+      },
+      {
+        "<c-.>",
+        function() require("sidekick.cli").send({ focus = true, msg = "{this}" }) end,
+        mode = { "n", "x" },
+        desc = "Sidekick Send This",
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "markdown" },
+        callback = function() vim.b.sidekick_nes = false end,
+      })
+    end,
   },
-  keys = {
-    {
-      "<M-l>",
-      function()
-        if require("sidekick").nes_jump_or_apply() then return end
-        if vim.lsp.inline_completion.get() then return end
-        return "<M-l>"
-      end,
-      expr = true,
-      desc = "Goto/Apply Next Edit Suggestion",
-      mode = { "i", "n" },
-    },
-    {
-      "<M-Right>",
-      function()
-        if require("sidekick").nes_jump_or_apply() then return end
-        if vim.lsp.inline_completion.get() then return end
-        return "<M-Right>"
-      end,
-      expr = true,
-      desc = "Goto/Apply Next Edit Suggestion",
-      mode = { "i", "n" },
-    },
-    {
-      "<c-.>",
-      function() require("sidekick.cli").send({ focus = true, msg = "{this}" }) end,
-      mode = { "n", "x" },
-      desc = "Sidekick Send This",
-    },
+  -- Amp CLI Integration
+  {
+    "sourcegraph/amp.nvim",
+    branch = "main",
+    lazy = false,
+    opts = { auto_start = true, log_level = "info" },
   },
-  init = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "markdown" },
-      callback = function() vim.b.sidekick_nes = false end,
-    })
-  end,
 }
