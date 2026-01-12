03245840e4ab8640e8227c85b764ebcc3c29a985
David Guevara <datwaftx@gmail.com>
2026-01-11 21:44:30 -0600

    fix(ts_query_ls): use proper parser install directory

--- init.lua
+++ init.lua
@@ -438,10 +438,7 @@ vim.lsp.config("vtsls", {
 vim.lsp.config("ts_query_ls", {
   settings = {
     parser_install_directories = {
-      vim.fn.stdpath("data") .. "/lazy/nvim-treesitter/parser/",
-    },
-    language_retrieval_patterns = {
-      "languages/src/([^/]+)/[^/]+\\.scm$",
+      vim.fn.stdpath("data") .. "/site/parser/",
     },
   },
 })
