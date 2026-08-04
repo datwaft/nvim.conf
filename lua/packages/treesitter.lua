741f6388d8eece0a9ae0e15eef7353972b8957aa
David Guevara <datwaftx@gmail.com>
2026-08-04 13:40:44 -0600

    feat: add bg3 modding grammar and lsp

--- lua/packages/treesitter.lua
+++ lua/packages/treesitter.lua
@@ -20,15 +20,28 @@ return {
     opts = {
       update_cadence = "weekly",
       ensure_installed = {
+        "bg3_stats",
+        "bg3_stats_value",
         "comment",
         "fennel",
         "ghactions",
         "ghostty",
+        "xml",
       },
       overrides = {
+        bg3_stats = {
+          url = "https://github.com/datwaft/tree-sitter-bg3",
+          location = "tree-sitter-bg3-stats",
+        },
+        bg3_stats_value = {
+          url = "https://github.com/datwaft/tree-sitter-bg3",
+          location = "tree-sitter-bg3-stats-value",
+        },
         comment = { url = "https://github.com/OXY2DEV/tree-sitter-comment" },
         ghactions = { url = "https://github.com/rmuir/tree-sitter-ghactions" },
       },
     },
   },
+  { "datwaft/tree-sitter-bg3", lazy = false },
+  { "datwaft/bg3.nvim", lazy = false },
 }
